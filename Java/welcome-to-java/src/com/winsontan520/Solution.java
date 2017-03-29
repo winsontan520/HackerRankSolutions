3000d1cdc57e95a9a630b153d5a1798d0c59ecac
Winson Tan BS <boonseng@sph.com.sg>
2017-03-29 17:25:45 +0800

    first solution

---
+++ Java/welcome-to-java/src/com/winsontan520/Solution.java
@@ -0,0 +1,11 @@
+package com.winsontan520;
+
+public class Solution {
+    /*
+    https://www.hackerrank.com/challenges/welcome-to-java
+    */
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}

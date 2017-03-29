f14b1c687c2b2cf1f781a1fa0bf22556b504ada3
Winson Tan BS <boonseng@sph.com.sg>
2017-03-29 17:56:47 +0800

    add 2nd solution

---
+++ Java/java-stdin-and-stdout-1/src/com/winsontan520/Solution.java
@@ -0,0 +1,21 @@
+package com.winsontan520;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    /*
+    https://www.hackerrank.com/challenges/java-stdin-and-stdout-1
+    */
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
